cf3dddb383426a31a10757964a7967676e219146
kondors1995 <normandija1945@gmail.com>
2024-12-27 14:50:27 +0000

    zumapro: Inital adaptation for yaap

--- BoardConfigYaap.mk
+++ BoardConfigYaap.mk
@@ -5,7 +5,7 @@
 #
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/yaap/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     device/google/zumapro/android.hardware.security.rkp-service.citadel.xml \
     device/google/zumapro/manifest_radio_ds.xml
@@ -33,7 +33,7 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor
 
 # Reserve space for gapps install
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/yaap/config/BoardConfigReservedSize.mk
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
